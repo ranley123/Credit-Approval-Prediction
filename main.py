bbf67482131a48ee9e40a43569bb1a6886b7d09f
Ranley Yu <ranley0109@gmail.com>
2021-04-13 16:56:51 +0800

    final

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ from sklearn.utils import shuffle
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import balanced_accuracy_score
-import seaborn as sns
 import matplotlib.pyplot as plt
-
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_precision_recall_curve
+import warnings
+warnings.filterwarnings("ignore")
 
 def KFoldSplit(X, y):
+    '''
+    This function would do K fold split and return each fold
+    '''
     kf = KFold()
     kf.get_n_splits(X)
 
@@ -27,18 +32,21 @@ def preprocess(filename, sep=','):
     # load data
     df = pd.read_csv(filename, sep=sep, header=None)
 
+    # give each column a name
     df.columns = ['A' + str((i + 1)) for i in range(df.shape[1])]
 
     # remove missing values
     for i in df.columns:
         df = df.drop(index=df[df[i].astype(str) =='?'].index, axis=0)
 
-    # shuffle
+    # shuffle before splitting
     df = shuffle(df)
 
+    # get X and y respectively
     X = df.iloc[:, :-1]
     y = df.iloc[:, -1]
 
+    # those attributes are categorical, needed to be encoded
     indices = ['A1', 'A4', 'A5', 'A6', 'A7', 'A9', 'A10', 'A12', 'A13']
 
     for name in indices:
@@ -51,17 +59,6 @@ def preprocess(filename, sep=','):
         # Join the encoded df
         one_hot.columns = names
         X = X.join(one_hot)
-    
-    # select features
-    correlation_mat = df_small.corr()
-
-    sns.heatmap(correlation_mat, annot = True)
-
-    plt.show()
-
-    # normalise
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
 
     # encoding y
     y = y.astype("str")
@@ -69,52 +66,61 @@ def preprocess(filename, sep=','):
     y.loc[y == '-'] = '0'
     y = np.array(y.astype('int'))
 
-    return X, y
+    return X.to_numpy(), y
 
-def normal_train(X, y):
-    # train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    # correlation_mat = X_train.join(y_train).corr()
-    # print(correlation_mat['A16'])
-
-    # sns.heatmap(correlation_mat, annot = True)
-
-    # plt.show()
-    # A8, A11, A9, A10
-    # X_train = X_train[['A8', 'A11', 'A9_f', 'A9_t', 'A10_t', 'A10_f']]
-    # X_test = X_test[['A8', 'A11', 'A9_f', 'A9_t', 'A10_t', 'A10_f']]
-    clf = LogisticRegression(penalty='none', class_weight=None)
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    accuracy, balanced = eval(y_test, y_pred)
-
-def KFold_train(X, y):
-    accuracies = []
-    balanced_accuracies = []
+def train(X, y):
+    all_y_true = []
+    all_y_pred = []
+    all_y_pred_balanced = []
 
     for X_train, X_test, y_train, y_test in KFoldSplit(X, y):
-        clf = LogisticRegression()
+        # normalise
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.fit_transform(X_test)
+
+        clf = LogisticRegression(penalty='none', class_weight=None)
+        clf_balanced = LogisticRegression(penalty='none', class_weight='balanced')
+        
+        # fit the training set
         clf.fit(X_train,y_train)
+        clf_balanced.fit(X_train,y_train)
+
+        # get predicted values
         y_pred = clf.predict(X_test)
-        accuracy, balanced = eval(y_test, y_pred)
-        accuracies.append(accuracy)
-        balanced_accuracies.append(balanced)
-    print('Avg Accuracy: ', round(np.mean(accuracies), 3))
-    print('Avg Balanced Accuracy: ', round(np.mean(balanced_accuracies), 3))
+        y_pred_balanced = clf_balanced.predict(X_test)
+
+        # store predicted values
+        all_y_true.extend(y_test)
+        all_y_pred.extend(y_pred)
+        all_y_pred_balanced.extend(y_pred_balanced)
+
+        # plot for each fold
+        # plot_precision_recall_curve(clf, X_test, y_test).figure_.show()
+        # plot_precision_recall_curve(clf_balanced, X_test, y_test)
+    
+    plt.show()
+
+    # evaluate 
+    accuracy, balanced = eval(all_y_true, all_y_pred)
+    accuracy_balanced, balanced_balancend = eval(all_y_true, all_y_pred_balanced)
+    print('Normal LR Avg Accuracy: ', accuracy)   
+    print('Normal LR Avg Balanced Accuracy: ', balanced)
+    print('Balanced LR Avg Accuracy: ', accuracy_balanced)   
+    print('Balanced LR Avg Balanced Accuracy: ', balanced_balancend)
+    print('Normal Confusion Matrix: ', confusion_matrix(all_y_true, all_y_pred))     
+    print('Balanced Confusion Matrix: ', confusion_matrix(all_y_true, all_y_pred_balanced)) 
 
 
 def eval(y_test, y_pred):
     accuracy = round(accuracy_score(y_test, y_pred), 3)
     balanced_accu = round(balanced_accuracy_score(y_test, y_pred), 3)
-    
-    print('accuracy: ', accuracy)
-    print('balanced accuracy: ', balanced_accu)
 
     return accuracy, balanced_accu
 
 
 if __name__ == "__main__":
     X, y = preprocess("data/crx.data")
-    normal_train(X, y)
+    train(X, y)
 
 
